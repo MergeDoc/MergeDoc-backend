d2361a4673567b2d03d039b86b8f0ce677528a09
PPakSang <ppm5377@naver.com>
2022-07-25 01:32:07 +0900

    [#1] ESTABLISH : 프로젝트 환경 설정

---
+++ src/test/java/com/mergedoc/backend/MergeDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mergedoc.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergeDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
