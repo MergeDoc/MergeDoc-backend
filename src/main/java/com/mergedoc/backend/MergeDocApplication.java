36e5c9ef9f8c36008593ad5fda380d6f64dacce2
PPakSang <ppm5377@naver.com>
2022-07-25 01:32:07 +0900

    [#1] ESTABLISH : 프로젝트 환경 설정

---
+++ src/main/java/com/mergedoc/backend/MergeDocApplication.java
@@ -0,0 +1,13 @@
+package com.mergedoc.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergeDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MergeDocApplication.class, args);
+    }
+
+}
