64667eafc033dd5abd03764252d3b951aa6f6e70
PPakSang <ppm5377@naver.com>
2022-07-28 13:16:02 +0900

    FEAT : Page Domain Entity, Repository, Test 추가

---
+++ src/main/java/com/mergedoc/backend/page/entity/Page.java
@@ -0,0 +1,31 @@
+package com.mergedoc.backend.page.entity;
+
+import com.mergedoc.backend.Base.BaseEntity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Page extends BaseEntity{
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    public void changeName(String changeName) {
+        this.name = changeName;
+    }
+
+    @Builder
+    public Page(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/com/mergedoc/backend/repository/PageRepositoryTest.java
@@ -0,0 +1,67 @@
+package com.mergedoc.backend.repository;
+
+import com.mergedoc.backend.page.entity.Page;
+import com.mergedoc.backend.page.entity.PageUnit;
+import com.mergedoc.backend.page.repository.PageRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class PageRepositoryTest {
+
+    @Autowired
+    private PageRepository pageRepository;
+
+
+    Page page;
+
+    @BeforeEach
+    public void createPage() {
+        page = Page.builder()
+                .name("sample_page")
+                .build();
+
+        pageRepository.savePage(page);
+
+        //        DIR 에 추가
+
+    }
+
+    @Test
+    public void findPage() {
+        Page findPage = pageRepository.findPageById(page.getId());
+
+        assertThat(findPage).isEqualTo(page);
+    }
+
+    @Test
+    public void addPageUnit() {
+//        Unit 생성
+
+        PageUnit pageUnit = PageUnit.builder()
+                .page(page)
+                .build();
+//        Unit 매핑
+
+        pageRepository.savePageUnit(pageUnit);
+
+        List<PageUnit> findPageUnit = pageRepository.findPageUnitsByPage(page);
+
+        assertThat(findPageUnit.get(0)).isEqualTo(pageUnit);
+    }
+
+    @Test
+    public void changePageName() {
+        pageRepository.changePageName(page, "sample_page2");
+
+        assertThat(page.getName()).isNotEqualTo("sample_page");
+    }
+}
