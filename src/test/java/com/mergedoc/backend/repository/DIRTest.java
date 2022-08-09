7f2266bcc5cd27e89180ea71e741e03383faa6a0
PPakSang <ppm5377@naver.com>
2022-08-09 21:58:25 +0900

    Refactor: Member 추가에 따른 DIRRepository 수정 member 가 추가됨에 따라 path+name 이 더이상 식별자의 역할을 할 수 없기 때문에 member_id 를 포함한 조회 로직으로 코드를 전면적으로 수정합니다.

--- src/test/java/com/mergedoc/backend/repository/DIRTest.java
+++ src/test/java/com/mergedoc/backend/repository/DIRTest.java
@@ -5,6 +5,8 @@ import com.mergedoc.backend.dir.entity.PageInDIR;
 import com.mergedoc.backend.dir.entity.UnitInDIR;
 import com.mergedoc.backend.dir.repository.DIRRepository;
 import com.mergedoc.backend.exceptions.NotFoundException;
+import com.mergedoc.backend.member.entity.Member;
+import com.mergedoc.backend.member.repository.MemberRepository;
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -22,29 +25,40 @@ public class DIRTest {
 
     @Autowired
     private DIRRepository dirRepository;
+    @Autowired
+    private MemberRepository memberRepository;
 
     DIR dir;
+    Member member;
 
     String rootPath = "/";
     String rootName = "root";
 
     @BeforeEach
     public void createRoot() {
-        DIR rootDIR = DIR.builder().build();
+        member = Member.builder().build();
+        memberRepository.save(member);
 
-        rootDIR.setName(rootName);
-        rootDIR.setPath(rootPath);
+        DIR rootDIR = DIR.builder()
+                .member(member)
+                .path(rootPath)
+                .name(rootName)
+                .build();
+
+
 
         dirRepository.saveDIR(rootDIR);
 
         String createName = "testDIR";
 
-        DIR parentDIR = dirRepository.findDIRByPathAndName(rootPath, rootName)
+        DIR parentDIR = dirRepository.findMemberDIRByPathName(member.getId(), rootPath, rootName)
                 .orElseThrow(() -> new NotFoundException("디렉토리를 찾을 수 없습니다."));
 
-        DIR createdDIR = DIR.builder().parent(parentDIR).build();
-        createdDIR.setPath(rootPath+rootName+"/");
-        createdDIR.setName(createName);
+        DIR createdDIR = DIR.builder()
+                .parent(parentDIR)
+                .path(rootPath+rootName+"/")
+                .name(createName)
+                .build();
 
         dirRepository.saveDIR(createdDIR);
 
@@ -54,22 +68,23 @@ public class DIRTest {
 
     @Test
     public void findUnit() {
-        DIR parentDIR = dirRepository.findDIRByPathAndName(rootPath, rootName)
+        DIR parentDIR = dirRepository.findMemberDIRByPathName(member.getId(), rootPath, rootName)
                 .orElseThrow(() -> new NotFoundException("디렉토리를 찾을 수 없습니다."));
 
         String childPath = rootPath+rootName+"/";
 
         UnitInDIR createUnitInDIR = UnitInDIR.builder()
                 .dir(parentDIR)
+                .member(member)
+                .path(childPath)
+                .name("testUnit")
                 .build();
 
-        createUnitInDIR.setPath(childPath);
-        createUnitInDIR.setName("testUnit");
 //        Unit 연관관계 추가 필요
 
         dirRepository.saveUnitInDIR(createUnitInDIR);
 
-        UnitInDIR findUnit = dirRepository.findUnitByPathAndName(childPath, "testUnit")
+        UnitInDIR findUnit = dirRepository.findMemberUnitByPathName(member.getId(), childPath, "testUnit")
                 .orElseThrow(() -> new NotFoundException("유닛을 찾을 수 없습니다."));
 
         assertThat(findUnit).isEqualTo(createUnitInDIR);
@@ -77,34 +92,67 @@ public class DIRTest {
 
     @Test
     public void findPage() {
-        DIR parentDIR = dirRepository.findDIRByPathAndName(rootPath, rootName)
+        DIR parentDIR = dirRepository.findMemberDIRByPathName(member.getId(), rootPath, rootName)
                 .orElseThrow(() -> new NotFoundException("디렉토리를 찾을 수 없습니다."));
 
         String childPath = rootPath+rootName+"/";
 
         PageInDIR createPageInDIR = PageInDIR.builder()
                 .dir(parentDIR)
+                .member(member)
+                .path(childPath)
+                .name("testPage")
                 .build();
 
-        createPageInDIR.setPath(childPath);
-        createPageInDIR.setName("testPage");
 //        Unit 연관관계 추가 필요
 
         dirRepository.savePageInDIR(createPageInDIR);
 
-        PageInDIR findUnit = dirRepository.findPageByPathAndName(childPath, "testPage")
+        PageInDIR findUnit = dirRepository.findMemberPageByPathName(member.getId(), childPath, "testPage")
                 .orElseThrow(() -> new NotFoundException("페이지를 찾을 수 없습니다."));
 
         assertThat(findUnit).isEqualTo(createPageInDIR);
     }
 
     @Test
-    public void findDIR() {
-        DIR findDIR = dirRepository.findDIRByPathAndName("/root/", "testDIR")
+    public void findRootFiles() {
+        DIR findDIR = dirRepository.findMemberDIRByPathName(member.getId(), rootPath, rootName)
                 .orElseThrow(() -> new NotFoundException("디렉토리를 찾을 수 없습니다."));
 
-        assertThat(findDIR).isEqualTo(dir);
+        String childPath = rootPath+rootName+"/";
+
+        IntStream.rangeClosed(1, 5).forEach(i -> {
+            PageInDIR createPage = PageInDIR.builder()
+                    .member(member)
+                    .path("/root/")
+                    .name("page" + i)
+                    .dir(findDIR)
+                    .build();
+
+            dirRepository.savePageInDIR(createPage);
+        });
+
+        IntStream.rangeClosed(1, 5).forEach(i -> {
+            UnitInDIR createUnit = UnitInDIR.builder()
+                    .member(member)
+                    .path("/root/")
+                    .name("unit" + i)
+                    .dir(findDIR)
+                    .build();
+
+            dirRepository.saveUnitInDIR(createUnit);
+        });
+
+        List<PageInDIR> findPages = dirRepository.findAllMemberPagesByPath(member.getId(), childPath);
+        List<UnitInDIR> findUnits = dirRepository.findAllMemberUnitsByPath(member.getId(), childPath);
+
+        for (PageInDIR inDIR : findPages) {
+            System.out.println(inDIR.getName());
+        }
+
+        for (UnitInDIR inDIR : findUnits) {
+            System.out.println(inDIR.getName());
+        }
+
     }
-
-
 }
