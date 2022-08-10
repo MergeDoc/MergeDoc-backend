951525ee8b74348f21134ea0dffe958068ba9bf5
PPakSang <ppm5377@naver.com>
2022-08-10 15:46:06 +0900

    Refactor: InDIR 관련 Class 수정 MappedSuperClass 에서 연관관계도 정상적으로 상속되는 것을 확인 후에 해당 필드도 InDIR 클래스에 포함하고 Builder 를 수정하였습니다

--- src/main/java/com/mergedoc/backend/dir/entity/PageInDIR.java
+++ src/main/java/com/mergedoc/backend/dir/entity/PageInDIR.java
@@ -1,6 +1,5 @@
 package com.mergedoc.backend.dir.entity;
 
-import com.mergedoc.backend.Base.BaseEntity;
 import com.mergedoc.backend.member.entity.Member;
 import com.mergedoc.backend.page.entity.Page;
 import lombok.AccessLevel;
@@ -18,23 +17,15 @@ public class PageInDIR extends InDIR{
     @Id @GeneratedValue
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "dir_id")
-    private DIR dir;
-
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "page_id")
     private Page page;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private Member member;
-
     @Builder
     public PageInDIR(DIR dir, Page page, Member member, String path, String name) {
-        this.dir = dir;
         this.page = page;
-        this.member = member;
+        this.setMember(member);
+        this.setDir(dir);
         this.setName(name);
         this.setPath(path);
     }
