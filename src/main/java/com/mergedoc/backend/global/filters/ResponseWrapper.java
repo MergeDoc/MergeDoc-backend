ca6c63ab097f78012ff16012f16d5ec6134dfa52
HyungUk Ryu <hw0603@naver.com>
2022-08-20 17:07:56 +0900

    Feat: Logback 적용
    Known Bugs: Exception 명시적 throw 시에도 LoggingFilter에서 `response.getStatus()` 호출하면 `500`이 아닌 `200`이 반환됨

---
+++ src/main/java/com/mergedoc/backend/global/filters/ResponseWrapper.java
@@ -0,0 +1,11 @@
+package com.mergedoc.backend.global.filters;
+
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class ResponseWrapper extends ContentCachingResponseWrapper {
+    public ResponseWrapper(HttpServletResponse response) {
+        super(response);
+    }
+}
--- src/main/java/com/mergedoc/backend/test/TestController.java
+++ src/main/java/com/mergedoc/backend/test/TestController.java
@@ -4,14 +4,26 @@ import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
+@Slf4j
 public class TestController {
-
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
     @GetMapping("/logtest")
-    public void errorTest(@RequestParam @Nullable boolean error) throws Exception{
+    public String errorTest(@RequestParam @Nullable boolean error) throws Exception{
+        String test = org.springframework.core.SpringVersion.getVersion();
+        logger.trace("Trace Level 테스트");
+        logger.debug("DEBUG Level 테스트");
+        logger.info("INFO Level 테스트");
+        logger.warn("Warn Level 테스트");
+        logger.error("ERROR Level 테스트");
         if (error) {
+            logger.error("Exception 발생");
             throw new Exception("류형욱");
         }
+        return test;
     }
 }
